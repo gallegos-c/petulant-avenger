290328e7cd6f54d3a265b0c29b519e716a3a6701
kloosman <kloosman@kloosman.com>
2015-01-10 01:16:49 -0600

    added migrations

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class BaseConfig(object):
 	DEBUG = False
 	SECRET_KEY = '.\x12\xbc\xc04\xc76\xd0d\xa7NF\xb9\xe9uJ\xa4\x017\xed(\x07\xd7D'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	#print SQLALCHEMY_DATABASE_URI
 
 
 class DevelopmentConfig(BaseConfig):
--- db_create.py
+++ db_create.py
@@ -8,7 +8,8 @@ db.create_all()
 # Insert
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
-
+db.session.add(BlogPost("Flask", "learning Flask"))
+db.session.add(BlogPost("postgres", "We setup a local postgres instance."))
 
 # Commit the changes
 db.session.commit()
